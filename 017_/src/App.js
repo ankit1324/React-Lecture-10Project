d3060981c5d17e496a638c708c13caca088629ff
Ankit Chaudhary <82435644+ankit1324@users.noreply.github.com>
2023-10-31 09:08:59 +0000

    props passing

--- 017_/src/App.js
+++ 017_/src/App.js
@@ -1,13 +1,12 @@
-import { Header } from "./components/Header";
-import { TaskList } from "./components/TaskList";
-import './App.css';
+import Header from "./components/Header";
+import TaskList from "./components/TaskList";
+import "./App.css";
 
 function App() {
-
   return (
     <div className="App">
       <Header />
-      <TaskList />
+      <TaskList title="MyHabit" subTitle="Ankit" />
     </div>
   );
 }
