64cf67eb832e8f09563e8b963952dd2fb12d1457
Ankit Chaudhary <82435644+ankit1324@users.noreply.github.com>
2023-11-01 11:58:56 +0000

    access input at frontend

--- 026/src/components/AddTask.js
+++ 026/src/components/AddTask.js
@@ -2,19 +2,27 @@ import { useState } from "react";
 import "./AddTask.css";
 
 export const AddTask = () => {
-    const [taskValue, setTaskValue] = useState("");
+  const [taskValue, setTaskValue] = useState("");
 
-    // const handleChange = (event) => {
-    //     setTaskValue(event.target.value);
-    // }
+  const handeChange = (event) => {
+    setTaskValue(event.target.value);
+  };
 
   return (
     <section className="addtask">
-        <form>
-            <input onChange={(e) => setTaskValue(e.target.value)} type="text" name="task" id="task" placeholder="Task Name" autoComplete="off" />
-            <button type="submit">Add Task</button>
-        </form>
-        {/* <p>{taskValue.length}</p> */}
+      <form>
+        <label htmlFor="task">TaskName:</label>
+        <input
+          onChange={handeChange}
+          type="text"
+          name="task"
+          id="task"
+          placeholder="taskName"
+          autoComplete="off"
+        />
+        <button type="submit"> Add TASK</button>
+      </form>
+      <p>{taskValue}</p>
     </section>
-  )
-}
+  );
+};
