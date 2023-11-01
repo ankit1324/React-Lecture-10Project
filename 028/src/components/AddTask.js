71e58b14f2f144fdbaaa72d4276e3d783a43c76a
Ankit Chaudhary <82435644+ankit1324@users.noreply.github.com>
2023-11-01 12:39:02 +0000

    AddTask_INConsoleTest

--- 028/src/components/AddTask.js
+++ 028/src/components/AddTask.js
@@ -2,40 +2,52 @@ import { useState } from "react";
 import "./AddTask.css";
 
 export const AddTask = () => {
-    const [taskValue, setTaskValue] = useState("");
-    const [progress, setProgress] = useState(false);
+  const [taskValue, setTaskValue] = useState("");
+  const [progress, setProgress] = useState(false);
 
-    const handleChange = (event) => {
-        setTaskValue(event.target.value);
-    }
+  const handeChange = (event) => {
+    setTaskValue(event.target.value);
+  };
 
-    const handleReset = () => {
-        setTaskValue("");
-        setProgress(false);
-    }
+  const handleReset = () => {
+    setTaskValue("");
+    setProgress(false);
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const task = {
-            id: Math.floor(Math.random() * 10000),
-            name: taskValue,
-            completed: Boolean(progress)
-        }
-        handleReset();
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const task = {
+      id: Math.floor(Math.random() * 1000),
+      name: taskValue,
+      completed: Boolean(progress),
+    };
+    console.log(task);
+    handleReset();
+  };
 
   return (
     <section className="addtask">
-        <form onSubmit={handleSubmit}>
-            <input onChange={handleChange} type="text" name="task" id="task" placeholder="Task Name" autoComplete="off" value={taskValue} />
-            <select onChange={(event) => setProgress(event.target.value)} value={progress}>
-                <option value="false">Pending</option>
-                <option value="true">Completed</option>
-            </select>
-            <span onClick={handleReset} className="reset">Reset</span>
-            <button type="submit">Add Task</button>
-        </form>
-        <p>{taskValue}</p>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="task">TaskName:</label>
+        <input
+          onChange={handeChange}
+          type="text"
+          name="task"
+          id="task"
+          placeholder="taskName"
+          autoComplete="off"
+          value={taskValue}
+        />
+        <select onChange={(e) => setProgress(e.target.value)} value={progress}>
+          <option value={true}>Completed</option>
+          <option value={false}>Pending</option>
+        </select>
+        <span onClick={handleReset} className="reset">
+          Reset
+        </span>
+        <button type="submit"> Add TASK</button>
+      </form>
+      <p>{taskValue}</p>
     </section>
-  )
-}
+  );
+};
