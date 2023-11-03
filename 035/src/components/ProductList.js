57eadb55c4d6707afeb48ea8cccfa2a433a6a8a2
Ankit <ak0330255@gmail.com>
2023-11-03 16:22:19 +0530

    MakeUI_with_APIdata

--- 035/src/components/ProductList.js
+++ 035/src/components/ProductList.js
@@ -1,26 +1,29 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react";
+import { useState } from "react";
 
 export const ProductList = () => {
-    const [products, setProducts] = useState([]);
-    
-    useEffect(() => {
-        fetch("http://localhost:8000/products")
-        .then(response => response.json())
-        .then(data => setProducts(data));
-    }, []);
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/products")
+      .then((response) => response.json())
+      .then((data) => setProduct(data));
+  }, []);
 
   return (
     <section>
-        { products.map((product) => (
-            <div className="card" key={product.id}>
-                <p className="id">{product.id}</p>
-                <p className="name">{product.name}</p>
-                <p className="info">
-                    <span>${product.price}</span>
-                    <span className={product.in_stock ? "instock" : "unavailable"}>{product.in_stock ? "In Stock" : "Unavailable"}</span>
-                </p>
-            </div>            
-        )) }
+      {product.map((product) => (
+        <div className="card" key={product.id}>
+          <p className="id">{product.id}</p>
+          <p className="Name">{product.name}</p>
+          <p className="info">
+            <span>${product.price}</span>
+            <span className={product.in_stock ? "instock" : "unavailable"}>
+              {product.in_stock ? "In Stock" : "Unavailable"}
+            </span>
+          </p>
+        </div>
+      ))}
     </section>
-  )
-}
+  );
+};
