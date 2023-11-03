613bd573499c6e6d43ca1a8b3bd5a34677e12309
Ankit <ak0330255@gmail.com>
2023-11-03 16:04:10 +0530

    useEffect_Intro

--- 032/src/components/ProductList.js
+++ 032/src/components/ProductList.js
@@ -1,16 +1,15 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react";
+import { useState } from "react";
 
 export const ProductList = () => {
-    const [products, setProducts] = useState([]);
-    console.log(products);
-    
-    useEffect(() => {
-        fetch("http://localhost:8000/products")
-        .then(response => response.json())
-        .then(data => setProducts(data));
-    }, []);
+  const [product, setProduct] = useState([]);
+  console.log(product);
 
-  return (
-    <div>ProductList</div>
-  )
-}
+  useEffect(() => {
+    fetch("http://localhost:8000/products")
+      .then((response) => response.json())
+      .then((data) => setProduct(data));
+  }, []);
+
+  return <div>ProductList</div>;
+};
